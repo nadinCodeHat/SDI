db62294c82c1dfac3cdd5e96e8fd1c4e0d624ccf
nadinCodeHat <nadinpethiyagoda4@gmail.com>
2020-10-03 12:59:47 +0530

    Added file normal_distribution.py

--- Python/normal_distribution.py
+++ Python/normal_distribution.py
@@ -1,15 +1,16 @@
-import scipy.stats
-import numpy as np
+from numpy import random
 import matplotlib.pyplot as plt
+import seaborn as sns
 
-np.random.seed(1234)
-samples = np.random.lognormal(mean=1., sigma=.4, size=10000)
-shape, loc, scale = scipy.stats.lognorm.fit(samples, floc=0)
-num_bins = 50
-clr = "#EFEFEF"
-counts, edges, patches = plt.hist(samples, bins=num_bins, color=clr)
-centers = 0.5 * (edges[:-1] + edges[1:])
-cdf = scipy.stats.lognorm.cdf(edges, shape, loc=loc, scale=scale)
-prob = np.diff(cdf)
-plt.plot(centers, samples.size * prob, 'k-', linewidth=2)
+# μ - Mean Value
+mu = int(input("Input mean value: "))
+# σ - Standard Deviation
+sd = int(input("Input standard deviation"))
+
+x = random.normal(size=(mu, sd))
+# print the random normal distribution
+print(x)
+
+sns.distplot(random.normal(size=(mu, sd)), hist=False)
+# Plot values
 plt.show()
