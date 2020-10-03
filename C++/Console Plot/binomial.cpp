bc8f29c08050487ed4c2de3b7f9cc3dea2633be5
Chamath Wanigasooriya <chamathwanigasooriya@gmail.com>
2020-10-03 13:59:08 +0530

    added console plots in C++

---
+++ C++/Console Plot/binomial.cpp
@@ -0,0 +1,25 @@
+//Binomial distribution
+#include <iostream>
+#include <random>
+
+int main()
+{
+  const int nrolls = 10000; // number of experiments
+  const int nstars = 100;   // maximum number of stars to distribute
+
+  std::default_random_engine generator;
+  std::binomial_distribution<int> distribution(9,0.5);
+
+  int p[10]={};
+
+  for (int i=0; i<nrolls; ++i) {
+    int number = distribution(generator);
+    ++p[number];
+  }
+
+  std::cout << "binomial_distribution (9,0.5):" << std::endl;
+  for (int i=0; i<10; ++i)
+    std::cout << i << ": " << std::string(p[i]*nstars/nrolls,'*') << std::endl;
+
+  return 0;
+}
