f926d24b1c5035c298cd463a578c6ee2d2b0eb1a
Chamath Wanigasooriya <chamathwanigasooriya@gmail.com>
2020-10-03 12:11:20 +0530

    C++ boilerplate code added

---
+++ C++/SDIApp.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+//Prototype functions
+void binomialApproximation();
+void poissonApproximation();
+
+int main()
+{
+    return 0;
+}
+
+void binomialApproximation()
+{
+}
+
+void poissonApproximation()
+{
+}
